5299373b299bcb9a9aa4ad8a25c347b26ef5d029
scotwhite92 <scotwhite92@gmail.com>
2024-03-04 21:12:52 -0600

    extract text data

--- scrapr.py
+++ scrapr.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor
 from PIL import Image
 from tqdm import tqdm
+import sqlite3
+import json
 
 CARDS_API_URL = 'https://marvelsnapzone.com/getinfo/?searchtype=cards&searchcardstype=true'
 LOCATIONS_API_URL = 'https://marvelsnapzone.com/getinfo/?searchtype=locations&searchcardstype=true'
@@ -110,11 +112,57 @@ if __name__ == '__main__':
     locations = get_cards(LOCATIONS_API_URL)
     location_image_urls = [location['art'] for location in locations]
 
+    card_name = [card['name'] for card in cards]
+    card_cost = [card['cost'] for card in cards]
+    card_power = [card['power'] for card in cards]
+    card_ability = [card['ability'] for card in cards]
+    card_flavor = [card['flavor'] for card in cards]
+
+    card_ability_pruned = []
+    card_description = []
+    card_dictionary = []
+
+    for x in range(len(card_ability)):
+        card_ability_pruned.append(card_ability[x].replace("<span>", "").replace("</span>", ""))
+
+    for x in range(len(card_ability_pruned)):
+        if card_ability_pruned[x] == '':
+            card_description.append(card_flavor[x])
+        else:
+            card_description.append(card_ability_pruned[x])
+
+    for x in range(len(card_name)):
+        temp = [card_name[x],card_cost[x],card_power[x],card_description[x]]
+        card_dictionary.append(temp)
+    
+    if os.path.isfile("./cards.json"):
+        os.remove("./cards.json")
+
+    column = ['name', 'cost', 'power', 'description']
+    items = [dict(zip(column, row)) for row in card_dictionary]
+
+    with open("cards.json", "w") as f:
+        print(json.dumps(items), file=f)
+    
+    if os.path.isfile("./MarvelSnap.sqlite3"):
+        os.remove("./MarvelSnap.sqlite3")
+
+    con = sqlite3.connect("MarvelSnap.sqlite3")
+    cur = con.cursor()
+
+    with con:
+        cur.execute("CREATE TABLE cards(name, cost, power, description)")
+   
+    with con:
+        cur.executemany("INSERT INTO cards VALUES(?, ?, ?, ?)", card_dictionary)
+    
+    
+    con.close()
     create_directories()
 
-    download_images(card_image_urls, os.path.join(ROOT_DIR, CARDS_DIR))
-    download_images(variant_image_urls, os.path.join(ROOT_DIR, VARIANTS_DIR))
-    download_images(location_image_urls, os.path.join(ROOT_DIR, LOCATIONS_DIR))
+    #download_images(card_image_urls, os.path.join(ROOT_DIR, CARDS_DIR))
+    #download_images(variant_image_urls, os.path.join(ROOT_DIR, VARIANTS_DIR))
+    #download_images(location_image_urls, os.path.join(ROOT_DIR, LOCATIONS_DIR))
 
     print("[%s] %s" %
           (datetime.now(), f"Finished downloading. Check '{ROOT_DIR}' directory."))
